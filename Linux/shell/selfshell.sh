1743e5e3e353cb103938dbcad127839c27704833
sokmin <735305610@qq.com>
2018-01-18 18:33:54 +0800

    change shell info

--- Linux/shell/selfshell.sh
+++ Linux/shell/selfshell.sh
@@ -21,10 +21,15 @@ then
   #exit 终止脚本
 fi
 echo '请输入你的命令为:'
+#检测输入的命令
 read aNum
-case $aNum in
+checkPhp=`expr index "${aNum}" php`
+checkNg=`expr index "${aNum}" ng`
+#nginx运行函数
+selfNginx(){
+  case $1 in
     ng)  /usr/local/nginx/sbin/nginx
-	  echo 'nginx 启动成功'
+    echo 'nginx 启动成功'
     ;;
     ng-r) /usr/local/nginx/sbin/nginx -s reload
     echo 'nginx 重启成功'
@@ -32,16 +37,32 @@ case $aNum in
     ng-s) /usr/local/nginx/sbin/nginx -s stop 
     echo 'nginx 停止成功'
     ;;
-    php) /usr/local/php/sbin/php-fpm
-	  echo 'php 启动成功'
-    ;;
-    php-r) kill `cat /usr/local/php/var/run/php-fpm.pid`
-	  /usr/local/php/sbin/php-fpm
-	  echo 'php 重启成功'
-    ;;
-    php-s) kill `cat /usr/local/php/var/run/php-fpm.pid`
-   	echo 'php 停止成功'
-    ;;
-    *) echo '你输入的命令无效'
-    ;;
-esac
+  esac
+  exit
+}
+#php运行函数
+selfPhp(){
+  case $1 in
+      php) /usr/local/php/sbin/php-fpm
+      echo 'php 启动成功'
+      ;;
+      php-r) kill `cat /usr/local/php/var/run/php-fpm.pid`
+      /usr/local/php/sbin/php-fpm
+      echo 'php 重启成功'
+      ;;
+      php-s) kill `cat /usr/local/php/var/run/php-fpm.pid`
+      echo 'php 停止成功'
+      ;;
+  esac
+  exit 
+}
+#运行函数
+if [ $checkPhp -gt  0 ]; then
+  selfPhp $aNum 
+fi
+if [ $checkNg -gt 0 ]; then
+  selfNginx $aNum
+fi
+#错误提示
+echo '无效命令'
+exit
