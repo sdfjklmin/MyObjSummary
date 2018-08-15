e3ca7bbb006a4472536554ac9ec1391a17bc3c00
sokmin <735305610@qq.com>
2018-08-15 17:11:38 +0800

    Signed-off-by: sokmin <735305610@qq.com>

--- php/password_hash().php
+++ php/password_hash().php
@@ -1,4 +1,5 @@
 <?php
+echo "<pre>";
 /*password_hash数据加密 具体请参照PHP官方文档*/
 $password_plaintext = [
     'name'=>'passwd',
@@ -8,12 +9,17 @@ $password_plaintext = [
 #操作参数(可省)
 $options = [
     'cost' => 11, # 对应的cost值算法默认为10,可以根据硬件情况进行设置
-    'salt' => mcrypt_create_iv(22, MCRYPT_DEV_URANDOM), # 加盐设置(建议省掉,函数中已经自动加盐)
+    //'salt' => mcrypt_create_iv(22, MCRYPT_DEV_URANDOM), # 加盐设置(建议省掉,函数中已经自动加盐)
 ];
 #使用加密password_hash('str|must',must);
 $password_hash = password_hash( json_encode($password_plaintext), PASSWORD_DEFAULT,$options);
 
 #获取加密信息
-password_get_info( $password_hash ) ;
+$data = password_get_info( $password_hash ) ;
+
 #解密验证 bool
 $check = password_verify(json_encode($password_plaintext),$password_hash) ;
+
+$check2 = password_verify($password_hash,$password_hash) ;
+
+var_dump($password_hash,$data,$check,$check2) ;exit();
