ab01ba47bd14d6354168953d3e633a4cc55c9cb1
sokmin <735305610@qq.com>
2018-10-01 16:56:44 +0800

    新增PHP生命周期Server Mesh
    Signed-off-by: sokmin <735305610@qq.com>

--- php/深入PHP内核/2/php_variable.c
+++ php/深入PHP内核/2/php_variable.c
@@ -32,25 +32,25 @@ typedef union _zvalue_value {
 } zvalue_value;
 
 // php实现的8中数据类型,具体内核实现请参考web
-IS_NULL  第一次初始化如果变量没有赋值,则会自动的被赋予这个常量
-IS_BOOL  true 和 false 
-IS_LONG  可以存储从 -2147483648 到 +2147483647 范围内的任一整数 ,如果超出不会溢出,会自动转换为 IS_DOUBLE
-IS_DOUBLE 	浮点数
-IS_STRING 	字符串,内核会保存字符自身的长度,储存的时候 自身长度+1 ,
-IS_ARRAY    聚集别的变量,它可以承载任意类型的数据,通过 hash table(哈希表) 来实现
-IS_OBJECT   对象也是用来存储复合数据的,但是与数组不同的是,对象还需要保存以下信息:方法,访问权限,类常量以及其它的处理逻辑
-IS_RESOURCE  一种资源,一些无法呈现给php用户的数据,比如数据库连接
-             var_dump(@mysql_connect('127.0.0.1' . ':' . '3306', 'root', '123456'))
+// IS_NULL  第一次初始化如果变量没有赋值,则会自动的被赋予这个常量
+// IS_BOOL  true 和 false
+// IS_LONG  可以存储从 -2147483648 到 +2147483647 范围内的任一整数 ,如果超出不会溢出,会自动转换为 IS_DOUBLE
+// IS_DOUBLE 	浮点数
+// IS_STRING 	字符串,内核会保存字符自身的长度,储存的时候 自身长度+1 ,
+// IS_ARRAY    聚集别的变量,它可以承载任意类型的数据,通过 hash table(哈希表) 来实现
+// IS_OBJECT   对象也是用来存储复合数据的,但是与数组不同的是,对象还需要保存以下信息:方法,访问权限,类常量以及其它的处理逻辑
+// IS_RESOURCE  一种资源,一些无法呈现给php用户的数据,比如数据库连接
 
-
-@var_dump(is_null($undefined)) ;
-var_dump(is_bool(false))	;
-var_dump(is_long(223)) ;
-var_dump(is_double(232.23)) ;
-var_dump(is_string('23')) ;
-var_dump(is_array([])) ;
-@var_dump(is_object(new PDO('mysql:host=127.0.0.1;dbname=lcm;charset=UTF-8', 'root', '123456'))) ;
-var_dump(is_resource(@mysql_connect('127.0.0.1' . ':' . '3306', 'root', '123456'))) ;
+// Php Code
+// var_dump(@mysql_connect('127.0.0.1' . ':' . '3306', 'root', '123456'))
+// @var_dump(is_null($undefined)) ;
+// var_dump(is_bool(false))	;
+// var_dump(is_long(223)) ;
+// var_dump(is_double(232.23)) ;
+// var_dump(is_string('23')) ;
+// var_dump(is_array([])) ;
+// @var_dump(is_object(new PDO('mysql:host=127.0.0.1;dbname=lcm;charset=UTF-8', 'root', '123456'))) ;
+// var_dump(is_resource(@mysql_connect('127.0.0.1' . ':' . '3306', 'root', '123456'))) ;
 
 
 zval结构体里的type成员的值便是以上某个IS_*常量之一。 内核通过检测变量的这个成员值来知道他是什么类型的数据并做相应的后续处理。
