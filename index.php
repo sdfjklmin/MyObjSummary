264ae6747bbcb6631fe431310fce01b2ab04b653
sokmin <735305610@qq.com>
2018-11-27 15:01:28 +0800

    add

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
  * Date: 2018/11/22
  * Time: 11:41
  */
-
-require './comFunction/function.php';
-\MyObjSummary\comFunction\dd(23);
+//重定向页面 非index入口重定向到index
+//header('location:/index.php');
+//以index为入口,根据入口访问路由做路由分配
+echo phpinfo();
