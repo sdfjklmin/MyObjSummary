068ec899c993a7aa3f4cf7fb1e84f53f356a757c
sokmin <735305610@qq.com>
2018-11-28 15:37:12 +0800

    Signed-off-by: sokmin <735305610@qq.com>

--- CorePart.php
+++ CorePart.php
@@ -14,8 +14,14 @@ namespace MyObjSummary;
  */
 abstract class CoreInterpreter
 {
-    //这里没有明确的路由规则
-    //只是简单的配置了解释器
+    /** 路由过滤规则
+     * @return mixed
+     */
+    abstract protected function ruleFitter();
+
+    /**  解释器路由匹配
+     * @param $path
+     */
     public function interpreter($path)
     {
         switch ($path)
@@ -37,15 +43,31 @@ abstract class CoreInterpreter
 class CorePart extends CoreInterpreter
 {
     const PATH_NUM = 1 ;
-    public function run()
+
+    /** 规则实现
+     * @return mixed|null|string
+     */
+    protected function ruleFitter()
     {
         $path = trim($_SERVER['REQUEST_URI'],'/') ;
         if(empty($path)) {
-            return ;
+            return null;
         }
         $pathArr = explode('/',$path) ;
         if( count($pathArr) != self::PATH_NUM ) {
             exit(' this rule is forbid ');
+        }
+        return $path ;
+    }
+
+    /** 运行入口
+     * @return null
+     */
+    public function run()
+    {
+        $path = $this->ruleFitter();
+        if(!$path) {
+            return null ;
         }
         $this->interpreter($path);
         exit() ;
