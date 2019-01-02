4bd251b89b6732043058f6abacf0ad49353ca530
sokmin <735305610@qq.com>
2019-01-02 11:07:14 +0800

    update

--- phpunit.php
+++ phpunit.php
@@ -10,7 +10,10 @@ class TestUnit extends TestCase
 	public function testOne($value='')
 	{
 		# code...
+        echo "\n";
 		echo "one";
 	}
 }
+(new TestUnit())->testOne();
+//php phpunit.phar phpunit.php
 ?>
--- try.php
+++ try.php
@@ -1,7 +1,7 @@
 <?php
 //file_put_contents('xss.txt',json_encode($_REQUEST,true).PHP_EOL, FILE_APPEND) ;
 //特定测试文件
-use bookLog\buildApis\ChapterOne;
+use bookLog\buildApis\ChapterOne as bbCO;
 require './vendor/autoload.php';
-$t = new ChapterOne();
+$t = new bbCO();
 dd($t->build());
