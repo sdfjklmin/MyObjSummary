bf8ff20336d8eb336f27ea9e8ab1b53d49ad1662
sokminyo <735305610@qq.com>
2019-06-17 22:16:04 +0800

    update

---
+++ bookLog/designModel/Chapter01.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace bookLog\designModel;
+
+/**
+ * Class Chapter01
+ * @package bookLog\designModel
+ * 前言：
+ *  构建可复用的设计模式。（灵活、优雅、复用性更好）
+ *  不是解决所有的事情都要重头开始。可以借鉴已有成功的模式。
+ * 1.1 什么是设计模式
+ *  Christopher Alexander （建筑领域） ：
+ *      每一个模式描述了一个我们周围不断发生的问题，以及该问题的解决核心。
+ *      这样，你就能一次又一次地使用该方案而不是重复的劳动。
+ */
+class Chapter01
+{
+
+}
